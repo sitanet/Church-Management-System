b4baceab0772f5252deaf6eb833e5ca1596923e6
Bayo@DESKTOP-S33QV02 <sitanetglobaltech@gmail.com>
2023-08-26 11:42:39 +0100

    create template for all apps

--- coordinators/views.py
+++ coordinators/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def register_member(request):
-    return render(request, 'admin_staff/register_member.html')
+    return render(request, 'coordinators/register_member.html')
 
 
 def display_all_member(request):
-    return render(request, 'admin_staff/display_all_member.html')
+    return render(request, 'coordinators/display_all_member.html')
 
 
 def display_comment(request):
-    return render(request, 'admin_staff/display_comment.html')
+    return render(request, 'coordinators/display_comment.html')
 
 
 
 def new_comment(request):
-    return render(request, 'admin_staff/new_comment.html')
+    return render(request, 'coordinators/new_comment.html')
 
 
 
--- team_members/views.py
+++ team_members/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 # def register_member(request):
-#     return render(request, 'admin_staff/register_member.html')
+#     return render(request, 'team_members/register_member.html')
 
 
 def display_all_member(request):
-    return render(request, 'admin_staff/display_all_member.html')
+    return render(request, 'team_members/display_all_member.html')
 
 
 def display_comment(request):
-    return render(request, 'admin_staff/display_comment.html')
+    return render(request, 'team_members/display_comment.html')
 
 
 
 def new_comment(request):
-    return render(request, 'admin_staff/new_comment.html')
+    return render(request, 'team_members/new_comment.html')
 
 
 
